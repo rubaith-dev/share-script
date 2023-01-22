ae163b1719a5365f7014a41fca2b531831f85a07
Rubaith <dev.rubaith@gmail.com>
2023-01-22 18:06:05 +0600

    updated the file upload components

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ReactNode, ComponentType } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): ReactNode {
+
   return <Component {...pageProps} />
 }
