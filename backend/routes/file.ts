e11dd05cf1536212061226e7ce8e1cb7581db384
Rubaith <dev.rubaith@gmail.com>
2023-05-17 21:39:02 +0600

    upload route recieved chunks successfully

--- backend/routes/file.ts
+++ backend/routes/file.ts
@@ -1,10 +1,26 @@
 import express from 'express';
-import multer from 'multer';
-
+import { Request } from 'express';
 const router = express.Router();
+const multer = require('multer');
 
-router.post("/upload",(req,res)=>{
+// Create a multer storage configuration
+const storage = multer.memoryStorage();
 
+// Create a multer instance with the storage configuration
+const upload = multer({ storage });
+
+router.post("/upload",upload.single('data'), (req:Request,res)=>{
+    const { fileName, index, totalChunks } = req.body;
+  const chunk = req.file?.buffer;
+
+  console.log(`Received chunk for file ${fileName}`);
+  console.log(`Chunk index: ${index}`);
+  console.log(`Total chunks: ${totalChunks}`);
+  console.log(`Chunk data:`, chunk);
+
+  // Logic for processing the chunk and saving it
+
+  res.sendStatus(200); 
 })
 
 export default router
